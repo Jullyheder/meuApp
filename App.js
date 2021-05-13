ebc870312c78a92ea83cb7ef71c79be2388166a2
Jullyheder <38766649+Jullyheder@users.noreply.github.com>
2021-05-13 13:43:53 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  Image, 
+  TouchableOpacity, 
+} from 'react-native';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      textFrase: '',
+      img: require('./src/biscoito.png'),
+    };
+
+    this.abriBiscoito = this.abriBiscoito.bind(this);
+
+    this.frases = [
+      'A vida trará coisas boas se tiveres paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'A maior de todas as torres começa no solo.',
+      'Não há que ser forte. Há que ser flexível.',
+      'Gente todo dia arruma os cabelos, por que não o coração?',
+      'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+      'A juventude não é uma época da vida, é um estado de espírito.',
+      'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+      'Dê toda a atenção para a formação dos teus filhos, sobretudo por exemplos de tua própria vida.',
+      'Siga os bons e aprenda com eles.',
+      'Não importa o tamanho da montanha, ela não pode tapar o sol.',
+      'O bom-senso vai mais longe do que muito conhecimento.',
+      'Quem quer colher rosas deve suportar os espinhos.',
+      'São os nossos amigos que nos ensinam as mais valiosas lições.',
+      'Uma iniciativa mal-sucedida não significa o final de tudo. Sempre existe uma nova oportunidade.',
+      'Aquele que se importa com o sentimento dos outros, não é um tolo.',
+      'A adversidade é um espelho que reflete o verdadeiro eu.',
+      'Lamentar aquilo que não temos é desperdiçar aquilo que já possuímos.',
+    ];
+  }
+
+  abriBiscoito() {
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
+
+    this.setState({
+      textFrase: this.frases[numeroAleatorio],
+      img: require('./src/biscoitoAberto.png')
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image 
+          source={this.state.img}
+          style={styles.img}
+        />
+        <Text style={styles.text}>{this.state.textFrase}</Text>
+        <TouchableOpacity style={styles.button} onPress={this.abriBiscoito}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnText}>Abrir Biscoito</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    height: 250,
+    width: 250,
+  },
+  text: {
+    fontSize: 20,
+    color: '#dd7b22',
+    margin: 20,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  button: {
+    width: 230,
+    height: 50,
+    borderWidth: 2,
+    borderColor: '#dd7b22',
+    borderRadius: 25,
+  },
+  btnArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#dd7b22'
+  },
+})
+
+export default App;
